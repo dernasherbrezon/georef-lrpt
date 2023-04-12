562502f25a91eb103af19fe535369ab28005ba8c
dernasherbrezon <dernasherbrezon@gmail.com>
2023-04-12 17:24:53 +0100

    fix type + setup github actions build

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=dernasherbrezon_georef-lrpt&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=dernasherbrezon_georef-lrpt)
+
 # georef-lrpt
 Georeference images received via LRPT
--- src/main/java/ru/r2cloud/lrpt/model/CommandLineArgs.java
+++ src/main/java/ru/r2cloud/lrpt/model/CommandLineArgs.java
@@ -8,25 +8,25 @@ import ru.r2cloud.lrpt.FileValidator;
 
 public class CommandLineArgs {
 
-	@Parameter(names = "--tle-file", description = "File with TLE. Should be from the same data as VCDU", required = true, validateWith = FileValidator.class)
+	@Parameter(names = "--tle-file", description = "File with TLE. Should be from the same date as VCDU", required = true, validateWith = FileValidator.class)
 	private String tleFile;
 
 	@Parameter(names = "--vcdu-files", description = "Comma-separated files containing VCDU in the binary format. Example: ./*.vcdu", required = true)
 	private List<String> vcduFiles;
 
-	@Parameter(names = "--irotation-degrees", description = "An angle between satellite camera and nadir. Default: 2.6")
+	@Parameter(names = "--irotation-degrees", description = "An angle between satellite camera and nadir")
 	private Double irotationDegrees = 2.0;
 
-	@Parameter(names = "--jrotation-degrees", description = "An angle between satellite camera and nadir. Default: 0.6")
+	@Parameter(names = "--jrotation-degrees", description = "An angle between satellite camera and nadir")
 	private Double jrotationDegrees = 0.4;
 
-	@Parameter(names = "--output-dir", description = "Directory where to output the result. Default: current", required = true, validateWith = FileValidator.class)
+	@Parameter(names = "--output-dir", description = "Directory where to output the result", validateWith = FileValidator.class)
 	private String outputDir = ".";
 
-	@Parameter(names = "--start-time", description = "Time in UTC timezone of the first VCDU file. Format: yyyy-MM-dd HH:mm:ss")
+	@Parameter(names = "--start-time", description = "Time in UTC timezone of the first VCDU file. If not provided, then guess the time from the first vcdu filename. Format: yyyy-MM-dd HH:mm:ss")
 	private String startTime;
 
-	@Parameter(names = "--orekit-path", description = "Path where to find orekit urls or where to download them into. Default: current")
+	@Parameter(names = "--orekit-path", description = "Path where to find orekit urls or where to download them into")
 	private String orekitPath = ".";
 
 	@Parameter(names = "--orekit-urls", description = "Comma-separated urls where to download orekit master data. Default: https://gitlab.orekit.org/orekit/orekit-data/-/archive/master/orekit-data-master.zip,https://r2cloud.s3.amazonaws.com/dist/orekit-data-master.zip")
